6a807891af4f422e5c4c0f51350864bf132e3a89
spanieldaniel98 <damq1g17@soton.ac.uk>
2019-05-20 22:36:27 +0100

    Update Main.java

--- Challenge 1/Main.java
+++ Challenge 1/Main.java
@@ -2,8 +2,8 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		// Run the program
 		IDToNameConverter i = new IDToNameConverter();
-		i.getNameFromID();
-		i.getRelatedPeopleFromID();
+		//i.getNameFromID();
+		//i.getRelatedPeopleFromID();
 		i.getHomePageURL();
 	}
 }
