61f5cefc30e8ec093f6b96c496f8ef30e230defd
Daniel Queiros <damq1g17@soton.ac.uk>
2019-05-22 14:04:13 +0100

    commented

--- Challenge 1/AnagramGetter.java
+++ Challenge 1/AnagramGetter.java
@@ -18,15 +18,19 @@ public class AnagramGetter {
 		nameAnagrams = new ArrayList<String>();
 	}
 	
+	// Asks the user for a name, then prints and returns a list of anagrams thereof
 	public ArrayList<String> getAnagrams() throws MalformedURLException, Exception {
+		
+		// Prompts user for name
 		System.out.print("Enter a name: ");
 		name = scanner.nextLine();
 		
-		// Instantiates search query URL
+		// Instantiates search query URL and buffered reader
 		queryName = name.replaceAll(" ", "+");
 		queryURL = new URL("https://new.wordsmith.org/anagram/anagram.cgi?anagram=" + queryName + "&t=500&a=n");
 		reader = new BufferedReader(new InputStreamReader(queryURL.openStream()));
 		
+		// Finds anagrams for the name in the Wordsmith page source code, prints them and adds them to ArrayList
 		while((line = reader.readLine()) != null) {
 			if(line.contains("wait")) {
 				line = reader.readLine();
@@ -40,14 +44,19 @@ public class AnagramGetter {
 			}
 		}
 		
+		// If no anagrams were found for the name tell the user such
 		if(nameAnagrams.isEmpty())
 			System.out.println("The name " + name + " has no anagrams...");
+		
+		// If anagrams were found for the name, print a list thereof
 		else {
 			System.out.println("The name " + name + " has the following anagrams:");
 			for(String anagram : nameAnagrams) {
 				System.out.println(anagram);
 			}
 		}
+		
+		// Returns ArrayList of anagrams
 		return nameAnagrams;	
 	}
 }
